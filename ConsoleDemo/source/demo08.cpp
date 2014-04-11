367e9ed22834a00442d14daef15df8abc41cdb38
yvan vander sanden <yvan@attr-x.net>
2014-04-11 23:22:35 +0200

    added sound occlusion callback functionality

---
+++ ConsoleDemo/source/demo08.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstdlib>
+#include "yse.hpp"
+#ifdef WINDOWS
+#include <conio.h>
+#else
+#include "wincompat.h"
+#endif
+
+Flt occludeValue = 0;
+YSE::sound sound;
+
+Flt occlusionFunction(const YSE::Vec& pos, const YSE::Vec& listener) {
+  // for simplicity's sake, we just check with a global var here.
+  // In reality you should do a raycast here to check these positions with
+  // the physx or bullet implementation in your game.
+  return occludeValue;
+}
+
+
+
+int main() {
+  if (!YSE::System().init()) goto exit;
+  YSE::System().occlusionCallback(occlusionFunction);
+
+  sound.create("contact.ogg", NULL, true).setOcclusion(true);
+
+  std::cout << "Use q/a to increase / decrease sound occlusion." << std::endl;
+  std::cout << "...or e to exit." << std::endl;
+
+  sound.play();
+
+  while (true) {
+    if (_kbhit()) {
+      char ch = _getch();
+      switch (ch) {
+      case 'q': occludeValue += 0.1; break;
+      case 'a': occludeValue -= 0.1; break;
+      case 'e': goto exit;
+      }
+    }
+
+    YSE::System().sleep(100);
+    YSE::System().update();
+  }
+
+exit:
+  YSE::System().close();
+  return 0;
+}
