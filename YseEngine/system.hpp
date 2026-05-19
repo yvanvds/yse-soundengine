0383c96f17122aaf663987308f2b9f3700d75456
yvan vander sanden <yvan@attr-x.net>
2026-05-19 19:49:11 +0200

    Release v2.1.1

--- YseEngine/system.hpp
+++ YseEngine/system.hpp
@@ -18,7 +18,7 @@
 #include <string>
 
 namespace YSE {
-  const std::string VERSION = "2.1.0";
+  const std::string VERSION = "2.1.1";
 
   /** @brief Signature of a user-supplied sound-occlusion callback.
    *
